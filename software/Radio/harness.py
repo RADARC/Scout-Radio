cddb6466402c9dd43b52610e133c4c1574f2f993
borzwatson <simon@pumukli.net>
2025-02-14 09:33:48 +0000

    Added unit test code

---
+++ software/Radio/harness.py
@@ -0,0 +1,132 @@
+"""
+test harness/library of test functions for si4735.
+Runs on CircuitPython or MicroPython
+"""
+#TODO Change to drive UI functions in the radio.scout module
+
+import time
+import si4735_CP as si4735
+from radarcplatform import circuitpython, micropython
+
+if circuitpython():
+    import board
+    from busio import I2C
+    from digitalio import DigitalInOut, Direction
+elif micropython():
+    from machine import Pin, I2C
+
+g_radio = None
+
+# grab a singleton radio
+def getradio():
+    """ return a singleton si4735 device """
+    global g_radio
+
+    if not g_radio:
+        if circuitpython():
+            reset_pin = DigitalInOut(board.GP17)
+            reset_pin.direction = Direction.OUTPUT
+            i2c = I2C(board.GP19, board.GP18)
+            i2c_address = 0x63
+
+            #
+            # FIXME
+            # If we don't do this, the torch flashes randomly
+            # whilst programming the si4735. Need to find out
+            # why this is happening - it shouldn't.
+            #
+            torch = DigitalInOut(board.GP16)
+            torch.direction = Direction.OUTPUT
+            torch.value = False
+
+        if micropython():
+            reset_pin = Pin(15, Pin.OUT, Pin.PULL_UP)
+            i2c = I2C(0, scl=Pin(5), sda=Pin(4))
+            i2c_address = 0x11
+
+        g_radio = si4735.SI4735(i2c, reset_pin, i2c_address)
+
+    return g_radio
+
+def testfm(radio, freq):
+#    radio.reset()
+    radio.setFM()
+    #radio.getFirmware()
+
+    radio.setRDSConfig(1,3,3,3,3)
+    radio.setFrequency(freq)
+
+def test(radio, freq):
+    testfm(radio, freq)
+
+def test2():
+    radio = getradio()
+
+    #freq = 9391
+    radio.reset()
+
+    addr = radio.get_device_i2c_address()
+
+    assert addr
+
+    print("Address is ",addr)
+    radio.patchPowerUp()
+    radio.downloadPatch()
+
+    radio.setSSB(2)
+    radio.setFrequency(14000)
+
+    radio.setSSBConfig(1, 0, 0, 1, 0, 1)
+
+    time.sleep(2)
+    radio.setSSBAudioBandwidth(2)
+    print("bandwidth 2")
+    time.sleep(2)
+    radio.setSSBAudioBandwidth(3)
+    print("bandwidth 3")
+    time.sleep(2)
+    radio.setSSBAudioBandwidth(4)
+    print("bandwidth 4")
+    time.sleep(2)
+    radio.setSSBAudioBandwidth(1)
+    print("bandwidth 1")
+    time.sleep(2)
+
+    radio.setAM()
+    radio.setFrequency(198)
+    time.sleep(2)
+
+    radio.setFM()
+    fwrev = radio.getFirmware()
+    print(fwrev)
+
+    radio.setRDSConfig(1,3,3,3,3)
+    radio.setFrequency(10420)
+
+def sigrssi(radio):
+
+    print(radio.getCurrentReceivedSignalQuality(0)["rssi"])
+
+    radio.getRDSStatus(0,0,0)
+    print(radio.station_name)
+    print(radio.station_text)
+
+def contrssi(radio):
+    while True:
+        sigrssi(radio)
+        time.sleep(1)
+
+def reportfirmware(radio):
+    fwrev = radio.getFirmware()
+    print(fwrev)
+
+# radio = getradio()
+# radio.reset()
+# fwrev = radio.getFirmware()
+# radio.patchPowerUp()
+# radio.downloadPatch()
+# freq=9700
+# radio.setRDSConfig(1,3,3,3,3)
+# test(radio, freq)
+# contrssi(getradio())
+
---
+++ software/Radio/test.py
@@ -0,0 +1,174 @@
+"""
+  A unit test framework for the scout radio project built on pyunit.
+  https://docs.python.org/3/library/unittest.html
+  It uses a 'testboard' object to communicate with scout radios ("targets")
+  Runs on host, full fat python.
+"""
+import unittest
+import time
+import testboard
+
+# hack - should be able to run unit tests in any order eventually
+#unittest.TestLoader.sortTestMethodsUsing = None
+
+TARGETFILES = ["radarcplatform.py", "harness.py", "si4735_CP.py"]
+
+SERIALPORT = "/dev/ttyACM0"
+
+def formatoutput(output):
+    """ format output from target board """
+    for line in output.decode().split("\r\n"):
+        print(line)
+
+# hack so setUp main contents only run once...lots of ways to do this
+board = None
+
+#
+# TODO none of the tests are using pyunit methods
+# * self.assertEqual
+# * self.assertFalse
+# * self.assertTrue
+# etc...
+#
+# We are just running a bunch of stuff and failing if we get
+# a traceback from the target device.
+#
+# https://docs.python.org/3/library/unittest.html
+#
+class Si4735test(unittest.TestCase):
+
+    def setUp(self):
+        """ copy test files to target; grab a radio in the repl for testing """
+
+        global board
+
+        if not board:
+            board = testboard.getboard(SERIALPORT)
+
+            # must get one
+            assert board
+
+            board.setfiles(TARGETFILES)
+
+            board.initialise()
+
+            #
+            # grab a singleton si4735 device as our first job
+            #
+            board.sendrepl('import harness')
+            text = board.sendrepl('radio = harness.getradio()')
+            formatoutput(text)
+
+    def test01(self):
+        """ reset radio """
+        text = board.sendrepl('radio.reset()')
+        formatoutput(text)
+
+    def test03(self):
+        """ test report firmware """
+        text = board.sendrepl("harness.reportfirmware(radio)")
+        formatoutput(text)
+
+    def test04(self):
+        """ patchPowerUp """
+        text = board.sendrepl("radio.patchPowerUp()")
+        formatoutput(text)
+
+    def test05(self):
+        """ downloadPatch """
+        text = board.sendrepl("radio.downloadPatch()")
+        formatoutput(text)
+
+    def test06(self):
+        """ tune an FM station """
+        #
+        # FIXME: improve quality by switching to radio 4
+        #
+        text = board.sendrepl("harness.testfm(radio, 10440)")
+        formatoutput(text)
+
+    def test07(self):
+        """ report RSSI """
+        text = board.sendrepl('radio.getCurrentReceivedSignalQuality(0)["rssi"]')
+        formatoutput(text)
+
+    def test08(self):
+        """ report RSSI and RDS """
+        text = board.sendrepl('harness.sigrssi(radio)')
+        formatoutput(text)
+
+    def test09(self):
+        """ set volume low-ish """
+        text = board.sendrepl('radio.setVolume(32)')
+        formatoutput(text)
+
+    def test10(self):
+        """ test ssb stuff """
+        #
+        # NOTE time.sleep's can be done either on host or target.
+        # probably host makes more sense?
+        #
+        text = board.sendrepl('radio.reset()')
+        formatoutput(text)
+
+        text = board.sendrepl('radio.patchPowerUp()')
+        formatoutput(text)
+
+        text = board.sendrepl('radio.downloadPatch()')
+        formatoutput(text)
+
+        text = board.sendrepl('radio.setSSB(2)')
+        formatoutput(text)
+
+        text = board.sendrepl('radio.setFrequency(14000)')
+        formatoutput(text)
+
+        text = board.sendrepl('radio.setSSBConfig(1, 0, 0, 1, 0, 1)')
+        formatoutput(text)
+
+        time.sleep(2)
+        text = board.sendrepl('radio.setSSBAudioBandwidth(2)')
+        formatoutput(text)
+
+        print("bandwidth 2")
+        time.sleep(2)
+        text = board.sendrepl('radio.setSSBAudioBandwidth(3)')
+        formatoutput(text)
+
+        print("bandwidth 3")
+        time.sleep(2)
+
+        text = board.sendrepl('radio.setSSBAudioBandwidth(4)')
+        formatoutput(text)
+        print("bandwidth 4")
+        time.sleep(2)
+
+        text = board.sendrepl('radio.setSSBAudioBandwidth(1)')
+        formatoutput(text)
+        print("bandwidth 1")
+        time.sleep(2)
+
+    #
+    # back to FM to finish with
+    #
+    def test11(self):
+        self.test01()
+        self.test06()
+
+    def test12(self):
+        self.test09()
+
+if __name__=="__main__":
+    unittest.main(failfast=True)
+
+#
+# need to install 'rshell' command line utility for micropython
+#
+# can invoke using
+# python -m unittest test.Si4735test
+# or
+# python -m unittest test.Si4735test.test6
+#
+# for example
+# or just plain old python test.py which is the __main__ case above
+#
