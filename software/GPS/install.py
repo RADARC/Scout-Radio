2a64cd7ec14107df40c7cd2cec5646fbc14981b9
John Gumb <john@gumb.org>
2025-03-11 14:41:46 +0000

    beginnings of installer for GPS - doesn't work due to adafruit_gps not being found

---
+++ software/GPS/install.py
@@ -0,0 +1,21 @@
+""" installer for GPS component """
+import sys
+import installer
+
+def files():
+    """ source files """
+    return [
+        "gps_displayio_simpletest.py",
+        "gps_simpletest.py",
+        # Can't have file named serial.py on host as this conflicts
+        # with host python implementation.
+        ("gps-serial.py", "serial.py"),
+        "gps_satellitefix.py" 
+      ]
+
+def homedir():
+    """ source directory """
+    return "GPS"
+
+if __name__=="__main__":
+    installer.installfiles(sys.argv, homedir(), files())
