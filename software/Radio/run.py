0f650d7b654e4a4438291c9453bedc7db231ee7b
John Gumb <john@gumb.org>
2025-03-16 15:33:04 +0000

    add run.py - test.py without the test slant. Useful for dev work?

---
+++ software/Radio/run.py
@@ -0,0 +1,65 @@
+"""
+run a test file - this file is derived from test.py
+"""
+
+import sys
+import testboard
+from testboard import formatoutput
+import install
+
+# hack - should be able to run unit tests in any order eventually
+#unittest.TestLoader.sortTestMethodsUsing = None
+
+SERIALPORT = "/dev/ttyACM0"
+
+if __name__=="__main__":
+
+    if len(sys.argv) > 1:
+        if "--install" in sys.argv[1:]:
+            DO_INSTALL = True
+        if "--revsync" in sys.argv[1:]:
+            DO_REVSYNC = True
+    else:
+        DO_INSTALL = False
+        DO_REVSYNC = False
+
+    BOARD = testboard.getboard(SERIALPORT)
+
+    # must get one
+    assert BOARD
+
+    BOARD.sethomedir(install.homedir())
+
+    # optionally install files
+    if DO_INSTALL or DO_REVSYNC:
+        BOARD.setfiles(install.files() + install.supportfiles())
+
+    # pull files back?
+    if DO_REVSYNC:
+        BOARD.revsync()
+        sys.exit(0)
+
+    # does copy files to target
+    BOARD.initialise()
+
+    #
+    # grab a singleton si4735 device as our first job
+    #
+    BOARD.sendrepl('import harness')
+    text = BOARD.sendrepl('radio = harness.getradio()')
+    formatoutput(text)
+
+    text = BOARD.sendrepl('radio.reset()')
+    formatoutput(text)
+
+    text = BOARD.sendrepl('radio.patchPowerUp()')
+    formatoutput(text)
+
+    text = BOARD.sendrepl('radio.downloadPatch()')
+    formatoutput(text)
+
+    text = BOARD.sendrepl('radio.setSSB(2)')
+    formatoutput(text)
+
+    text = BOARD.sendrepl('radio.setFrequency(14000)')
+    formatoutput(text)
