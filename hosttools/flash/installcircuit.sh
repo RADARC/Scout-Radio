05e01d3e326222a52a66567c3c4b83ae7d095382
John Gumb <john@gumb.org>
2025-03-20 19:23:44 +0000

    remove duplicate code

--- hosttools/flash/installcircuit.sh
+++ hosttools/flash/installcircuit.sh
@@ -2,30 +2,10 @@
 
 # helper shell script to install circuit python
 
-mountpoint()
-{
-    mount | grep $1 | awk '{print $3}'
-}
+. ./nuke.sh
 
-await_mount()
-{
-    while [ "$(mountpoint $1)" = "" ]; do
-        sleep 0.1
-    done
-}
+nuke
 
-echo "waiting for mountpoint - plug in USB with bootsel pressed until this continues"
-echo "BEWARE ALL FILES WILL BE DELETED ON THE MICROPYTHON BOARD"
-echo "Ignore any mount windows opening - or dismiss them"
-await_mount RPI-RP2
-echo "Copying flashnuke...."
-cp flash_nuke.uf2 $(mountpoint RPI-RP2)
-
-echo "sleeping briefly...."
-sleep 5
-
-echo "waiting for mountpoint RPI-RP2"
-await_mount RPI-RP2
 echo "Copying circuit python...."
 cp adafruit-circuitpython-raspberry_pi_pico-en_GB-9.2.4.uf2 $(mountpoint RPI-RP2)
 
