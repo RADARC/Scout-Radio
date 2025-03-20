67d9ac2f7ee0a0bdc4bd5a70e603ef4863855fc7
John Gumb <john@gumb.org>
2025-03-20 13:40:46 +0000

    friendlyise

--- hosttools/flash/installcircuit.sh
+++ hosttools/flash/installcircuit.sh
@@ -1,29 +1,34 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # helper shell script to install circuit python
 
 mountpoint()
 {
-    mount | grep RPI-RP2 | awk '{print $3}'
+    mount | grep $1 | awk '{print $3}'
 }
 
 await_mount()
 {
-    echo "waiting for mountpoint - plug in USB with bootsel pressed until this continues"
-    echo "BEWARE ALL FILES WILL BE DELETED ON THE MICROPYTHON BOARD"
-    while [ "$(mountpoint)" = "" ]; do
+    while [ "$(mountpoint $1)" = "" ]; do
         sleep 0.1
     done
 }
 
-await_mount
+echo "waiting for mountpoint - plug in USB with bootsel pressed until this continues"
+echo "BEWARE ALL FILES WILL BE DELETED ON THE MICROPYTHON BOARD"
+echo "Ignore any mount windows opening - or dismiss them"
+await_mount RPI-RP2
 echo "Copying flashnuke...."
-cp flash_nuke.uf2 $(mountpoint)
+cp flash_nuke.uf2 $(mountpoint RPI-RP2)
 
-echo "unplug and replug USB then any key to continue"
-read -n1 cont
+echo "sleeping briefly...."
+sleep 5
 
-await_mount
+echo "waiting for mountpoint RPI-RP2"
+await_mount RPI-RP2
 echo "Copying circuit python...."
-cp adafruit-circuitpython-raspberry_pi_pico-en_GB-9.2.4.uf2 $(mountpoint)
+cp adafruit-circuitpython-raspberry_pi_pico-en_GB-9.2.4.uf2 $(mountpoint RPI-RP2)
+
+echo "waiting for mountpoint CIRCUITPYTHON"
+await_mount CIRCUITPY
 echo "Copying circuit python....done"
