9f60ce75db35ac0a3368932df1f2b705d33642bd
John Gumb <john@gumb.org>
2025-03-21 16:54:04 +0000

    comments

--- reinstallsys.sh
+++ reinstallsys.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env sh
 
+# rebuild scout radio system from scratch
+
+#
+# install circuit python
+#
 if ! hosttools/flash/installcircuit.sh; then
     echo "flash failed"
     exit 1
 fi
 
+#
+# install all Scout Radio stuff
+#
 export PYTHONPATH=$(pwd)/hosttools
 cd software
 python3 sysinstall.py
-
