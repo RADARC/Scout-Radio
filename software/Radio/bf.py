f5215f96413b940cbed1a14c63a10e4e0fe55006
John Gumb <john@gumb.org>
2025-03-22 11:29:14 +0000

    beginnings of a bitfield thing

---
+++ software/Radio/bf.py
@@ -0,0 +1,67 @@
+#
+# based on https://forum.micropython.org/viewtopic.php?t=5274
+#
+class bitfield():
+    def __init__(self, bytes):
+        self.bits = bytearray(bytes)
+        self.m_bitnames = {}
+
+    def fielddef(self, bitname, bitpos, bitlen=1, bitdefval=0):
+        self.m_bitnames[bitname] = (bitpos, bitlen, bitdefval)
+
+    def transitem(self, n):
+        # manual length specified
+        if isinstance(n, tuple):
+            (bitid, fieldlen) = n
+        else:
+            bitid = n
+            fieldlen = 1
+
+        if isinstance(bitid, str):
+            (bpos, bitlen, bitdefval) = self.m_bitnames[bitid]
+        else:
+            bpos = bitid
+            bitlen = fieldlen
+            bitdefval = 99
+
+        return (bpos, bitlen, bitdefval)
+
+    def __getitem__(self, n):
+        (bpos, fieldlen, _bitdefval) = self.transitem(n)
+
+        startval = self.bits[0] >> bpos
+
+        acc = 0
+        for i in range(fieldlen):
+            acc += startval & (1 << i)
+
+        return acc
+
+
+    def __setitem__(self, n, vv):
+        (bpos, fieldlen, _bitdefval) = self.transitem(n)
+
+        for i in range(fieldlen):
+            v = vv & (1 << i)
+
+            if v:
+                self.bits[0] |= v << bpos
+            else:
+                self.bits[0] &= ~(v << bpos)
+
+
+if __name__ == "__main__":
+    b = bitfield(1)
+    b.fielddef("byte",0,8)
+    b.fielddef("field0",0,2)
+    b.fielddef("field2",1,1)
+
+    b.fielddef("topnibble",4,4)
+
+    b["field0"]=3
+    b["topnibble"]=15
+    print(b[0])
+    print(b[0, 2])
+    print(b["byte"])
+
+    print(b["field2"])
